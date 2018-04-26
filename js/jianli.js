1d00292f0330815d1dcd51eefdda357d8f4d0a86
xiaowubing <1432927431@qq.com>
2018-04-26 22:51:50 +0800

    第四次

--- js/jianli.js
+++ js/jianli.js
@@ -20,32 +20,43 @@ $(function () {
     });
 
 
-    $(".top>.left>i").click(function(){
+    $(".top>.left").click(function(){
 
-        $(".mysilde").css("display", "block").animate({
-            left: 0,
-        }, 200,function(){
-            $(".flag").addClass("ying");
-        })
-        
+        // $(".mysilde").css("display", "block").animate({
+        //     left: 0,
+        // }, 200,function(){
+        //     $(".flag").addClass("ying");
+        // })
 
-    })
-
-
-    $("#main").click(function(e){
-
-        if(e.target !== $(".top>.left>i")[0] && e.target !==  $(".mysilde")[0] && e.target !== $(".top>.left>i")[1]
-       && e.target !== $(".top>.left>i")[2] && e.target !== $(".top>.left>i")[3] && e.target !== $(".top>.left>i")[4]){
-            $(".mysilde").animate({
-                left: "-100%",
-            }, 200,function(){
-                $(".mysilde").css("display", "none");
-                $(".flag").removeClass("ying");
-            })
+        if($("#main").hasClass("mainmove")){
+            $("#main").removeClass("mainmove")
+            $("#main").addClass("fanmove")  
+        }else {
+            $("#main").removeClass("fanmove")
+            $("#main").addClass("mainmove")
             
         }
 
+
+    
     })
+
+
+
+    // $("#main").click(function(e){
+
+    //     if(e.target !== $(".top>.left>i")[0] && e.target !==  $(".mysilde")[0] && e.target !== $(".top>.left>i")[1]
+    //    && e.target !== $(".top>.left>i")[2] && e.target !== $(".top>.left>i")[3] && e.target !== $(".top>.left>i")[4]){
+    //         $(".mysilde").animate({
+    //             left: "-100%",
+    //         }, 200,function(){
+    //             $(".mysilde").css("display", "none");
+    //             $(".flag").removeClass("ying");
+    //         })
+            
+    //     }
+
+    // })
 
 
 
